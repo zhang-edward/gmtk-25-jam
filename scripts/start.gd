a85be5b5c38e6766c170b45f5e4f1cfd67b7481d
david8zhang <david8zhang@berkeley.edu>
2025-08-03 02:37:38 -0700

    added tutorial

--- scripts/start.gd
+++ scripts/start.gd
@@ -1,10 +1,15 @@
 class_name Start
 extends Node2D
 
-@onready var button = $CanvasLayer/Button as Button
+@onready var start = $CanvasLayer/Button as Button
+@onready var how_to_play = $CanvasLayer/Button2 as Button
 
 func _ready() -> void:
-	button.pressed.connect(go_to_game)
+	start.pressed.connect(go_to_game)
+	how_to_play.pressed.connect(go_to_tutorial)
 
 func go_to_game():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func go_to_tutorial():
+	get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
